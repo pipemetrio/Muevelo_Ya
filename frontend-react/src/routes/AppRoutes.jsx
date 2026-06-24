ab2f17951f8bb65b778e6f72908bdd1078a3ae65
Sebastian_Ramirez_Castrillon <sebas.ramirez.0115@gmail.com>
2026-06-24 00:09:38 -0500

    refactor(routes): cambiar ruta home de / a /inicio con redirect

--- frontend-react/src/routes/AppRoutes.jsx
+++ frontend-react/src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "../pages/Home";
 import Login from "../pages/Login";
@@ -8,7 +8,8 @@ import Profile from "../pages/Profile";
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigate to="/inicio" replace />} />
+      <Route path="/inicio" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/profile" element={<Profile />} />
